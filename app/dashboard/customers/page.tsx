1de013f4260b64f1a2f81c34a5792e687b474ec2
Sulayman-ma <suleyman.abba@gmail.com>
2023-12-13 16:54:59 +0100

    inital commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,9 @@
+import { CardsSkeleton } from "@/app/ui/skeletons";
+
+export default function Page() {
+  return (
+    <>
+      <CardsSkeleton />
+    </>
+  );
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,9 @@
+import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
+
+export default function Page() {
+  return (
+    <>
+      <InvoicesTableSkeleton />
+    </>
+  );
+}
